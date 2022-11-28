c7082f31c4e74726636c47cbccb1290ba2a85d8f
jcgouleau <jean-charles.gouleau@savoye.com>
2022-11-28 16:13:44 +0100

    add team component

---
+++ jest.config.js
@@ -0,0 +1,30 @@
+const { pathsToModuleNameMapper } = require('ts-jest');
+
+const { compilerOptions } = require('./tsconfig');
+
+
+module.exports = {
+  "modulePaths": [
+    "<rootDir>"
+  ],
+  preset: 'jest-preset-angular',
+
+  roots: ['<rootDir>/src/'],
+
+  testMatch: ['**/+(*.)+(spec).+(ts|js)'],
+
+  setupFilesAfterEnv: ['<rootDir>/src/test.ts'],
+
+  collectCoverage: true,
+
+  coverageReporters: ['html'],
+
+  coverageDirectory: 'coverage/my-app',
+
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths || {}, {
+
+    prefix: '<rootDir>/',
+
+  }),
+
+};
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddTeamComponent } from './add-team/add-team.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddTeamComponent
   ],
   imports: [
     BrowserModule,
